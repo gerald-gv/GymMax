5fced9a3dd7c7ee58c72dc46e903082168ed06ce
gerald-gv <geraldgutierrez918@gmail.com>
2026-07-25 00:41:00 -0500

    firts commit

---
+++ GymMax/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace GymMax.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
