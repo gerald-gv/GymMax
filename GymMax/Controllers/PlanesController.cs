00200d02bc71ba8f726127305addf91c4da4aef4
gerald-gv <geraldgutierrez918@gmail.com>
2026-07-26 11:02:41 -0500

    feat: improve public gym pages and dynamic home content

---
+++ GymMax/Controllers/PlanesController.cs
@@ -0,0 +1,21 @@
+﻿using GymMax.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymMax.Controllers {
+    public class PlanesController : Controller {
+        private readonly AppDbContext _context;
+
+        public PlanesController(AppDbContext context) {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() {
+            var planes = await _context.Planes
+                .Where(p => p.Activo)
+                .ToListAsync();
+
+            return View(planes);
+        }
+    }
+}
